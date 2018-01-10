fb7db3ec407e1284473c511d6fc3058856628d8f
Zak Jones <jazdrv@gmail.com>
2018-01-09 21:32:07 -0600

    some more logic setup for the prep steps

--- bin/redux2/redux2.py
+++ bin/redux2/redux2.py
@@ -98,7 +98,7 @@ def go_backup():
     # }}}
 
     if config['make_report'] > 0:
-        print "MAKING REPORT..."
+        #print "MAKING REPORT..."
         if os.path.exists("report.csv"):
             os.remove("report.csv")
     
@@ -108,10 +108,78 @@ def go_backup():
     print "** + backup done."
 
 def go_prep():
+
     print "** prepare file structure."
+
+    # Check ZIPDIR - the folder containing the zip files exists {{{
+
+    if not os.path.exists(config['zip_dir']):
+        print "Input zip folder does not appear to exist. Aborting.\n"
+        sys.exit()
+
+    # }}}
+    # Check WORKING - the zip working exists && Empty it, otherwise make it {{{
+
+    if not os.path.exists('working'):
+        os.makedirs("working")
+    else:
+        files = glob.glob('working/*')
+        for f in files:
+            os.remove(f)
+
+    # }}}
+    # Check UNZIP - the folder for the output exists && Empty it, otherwise make it {{{
+
+    if not os.path.exists('unzip'):
+        os.makedirs("unzip")
+    else:
+        if config['zip_update_only'] == 0:
+            files = glob.glob('unzip/*.bed')
+            for f in files:
+                os.remove(f)
+            files = glob.glob('unzip/*.vcf')
+            for f in files:
+                os.remove(f)
+
+    # }}}
+    # Get the list of input files {{{
+
+    files = glob.glob('zip/big*.zip')
+
+    if len(files) == 0:
+        print "No input files detected in zip folder. Aborting."
+        print "Check name format: should be bigy-<NAME>-<NUMBER>.zip\n"
+        sys.exit()
+    else:
+        print "input files detected: " + str(files)
+
+    # }}}
+    # Check whether unzip is installed {{{
+
+    # !!!
+    print "fix the zip call here"
+    # command -v unzip >/dev/null 2>&1 || { echo >&2 "Unzip package not found. Aborting."; exit 1; }
+
+    # }}}
+    # Check whether SNP list exists {{{
+
+    if not os.path.exists('snps_hg19.csv'):
+        print "SNP names file does not exist. Try:"
+        print "wget http://ybrowse.org/gbrowse2/gff/snps_hg19.csv -O snps_hg19.csv\n"
+        sys.exit()
+
+    # }}}
+    # Check whether merge-ignore list exists {{{
+
+    if not os.path.exists('merge-ignore.txt'):
+        open('merge-ignore.txt','w').close()
+
+    # }}}
+
     print "** + prep done."
     
 def go_data():
+
     print "** process SNP data."
     print "** + SNP processing done."
 
