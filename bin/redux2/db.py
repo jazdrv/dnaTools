94c86adad0f0f0f6dca9c3a466153e77ca710aef
Zak Jones <jazdrv@gmail.com>
2018-01-15 13:35:57 -0600

    remove sort.py

--- bin/redux2/db.py
+++ bin/redux2/db.py
@@ -94,4 +94,16 @@ class DB(object):
         
     def insert_sort_data(self):
         foo = 2
+        #sorted_kits = select count(positive), kit_id
+        #    sorted_positive_variants(kit_id),sorted_negative_variants(kid_id)
+        #    from calls
+        #    where positive is True
+        #    group by positive
+        #    order by 1
+        #
+        #    for K in kits:
+        #       ...
+        #
+        #sort_positive_variants(kit_id)
+
 
