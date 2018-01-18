438b0a0a9c8d440a5416d62da26e67076ded739d
Zak Jones <jazdrv@gmail.com>
2018-01-18 10:18:51 -0600

    tweaking of chkRefTbls.sh

--- src/chkRefTbls.sh
+++ src/chkRefTbls.sh
@@ -3,11 +3,32 @@
 cd $REDUX_ENV
 
 DB="variant"
+info="c_variants|pos v1_hg19|grch37 v1_hg38|grch38 variants|pos"
 
+for I in `echo $info`; do
+tbl=`echo $I|cut -d'|' -f1`
+fld=`echo $I|cut -d'|' -f2`
+echo "-------------------"
+echo $tbl
+echo "-------------------"
+echo "select * from $tbl order by $fld limit 5;"
 sqlite3 $DB.db <<!
 .header on
 .mode column
-select * from c_variants order by pos limit 5;
-select * from v1_hg19 order by grch37 limit 5;
-select * from v1_hg38 order by grch38 limit 5;
+select count(*) from $tbl;
+select * from $tbl order by $fld limit 5;
 !
+done
+
+for I in `echo $info`; do
+tbl=`echo $I|cut -d'|' -f1`
+fld=`echo $I|cut -d'|' -f2`
+echo "-------------------"
+echo $tbl
+echo "-------------------"
+echo "select * from $tbl order by $fld limit 5;"
+sqlite3 $DB.db <<!
+.schema $tbl
+!
+done
+
